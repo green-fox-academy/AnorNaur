5a8a14889b380bcfb9050aadd0c67902f0cd7109
AnorNaur <mkalnoky@yahoo.com>
2019-12-05 14:43:30 +0100

    dbi

---
+++ week01/Day04/define-basic-info.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+var MyName: string = "Kálnoky Mátyás";
+console.log(MyName);
+// Your age as a number
+var MyAge: number = 24;
+console.log(MyAge);
+// Your height in meters as a number
+var MyHeight: number = 1.89;
+console.log(MyHeight);
+// Whether you are married or not as a boolean
+var Married: boolean = false;
+console.log(Married);
