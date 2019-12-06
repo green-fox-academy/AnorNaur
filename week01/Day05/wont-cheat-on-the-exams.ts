71c8f24a0b3a6a11ad0883bebb54f11d07083b1a
AnorNaur <mkalnoky@yahoo.com>
2019-12-06 10:20:35 +0100

    added

---
+++ week01/Day05/wont-cheat-on-the-exams.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+let swear: string = "I won't cheat on the exam!"
+let aa: number = 0
+
+while (aa < 100) {
+    aa++
+    console.log(swear)
+}
