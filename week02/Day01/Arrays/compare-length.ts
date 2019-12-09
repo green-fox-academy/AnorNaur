bcb5f691d5a681a918f42c0f0f0d00bb80d693d1
AnorNaur <mkalnoky@yahoo.com>
2019-12-09 14:18:40 +0100

    compared lengths of lists

---
+++ week02/Day01/Arrays/compare-length.ts
@@ -0,0 +1,15 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log "p2 is longer" to the console if `secondList` has more elements than `firstList`
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+let firstListLength: number = firstList.length;
+let secondListLength: number = secondList.length;
+if (secondListLength > firstListLength) {
+    console.log('p2 is longer');
+}
+
+// console.log(firstListLength, secondListLength);
